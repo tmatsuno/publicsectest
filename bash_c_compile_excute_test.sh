003381ffec102b68348295622a83bfed509db835
MATSUNO Tomoo <1682239+tmatsuno@users.noreply.github.com>
2019-07-13 13:09:05 +0900

    add shell excute test and c compile with excute test.

---
+++ bash_c_compile_excute_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# bash_c_compile_execute_test.sh ver 0.1
+# Copyright 2019 @kon_cl
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "excuted..."
+
+cat - << EOF > ./sec_test.c
+#include<stdio.h>
+int main(void){
+printf("c source compiled and excuted...\n");
+return 0;
+}
+EOF
+
+echo ${CSOURCE}
+gcc -o sec_test ./sec_test.c
+./sec_test
+rm ./sec_test.c sec_test
